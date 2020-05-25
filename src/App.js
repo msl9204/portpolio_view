ca36d567ae8eb3a54bfb3b68683b9552331760af
Lee <letomok@gmail.com>
2020-05-26 00:43:09 +0900

    logo 추가와 media query apply

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Page from "./components/Pages";
+import GlobalStyle from "./components/Globalstyles";
 
 function App() {
     return (
         <React.Fragment>
+            <GlobalStyle />
             <Page />
         </React.Fragment>
     );
--- src/components/Pages.js
+++ src/components/Pages.js
@@ -1,80 +1,421 @@
-import React from "react";
+import React, { useRef } from "react";
 import Typical from "react-typical";
-import Grid from "@material-ui/core/Grid";
 import styled from "styled-components";
-import bgimage from "../images/priscilla-du-preez-ZvvYXz72yZI-unsplash.jpg";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
 
-const StyledDiv = styled.div`
-    overflow-y: scroll;
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import bgimage from "../images/priscilla-du-preez-ZvvYXz72yZI-unsplash.jpg";
+import react_native_img from "../images/reactNative.png";
+import python_img from "../images/python.png";
+import js_img from "../images/JS.png";
+import html_img from "../images/html.png";
+import bootstrap_img from "../images/bootstrap.png";
+import css_img from "../images/css.png";
+import django_img from "../images/django.png";
+import git_img from "../images/git.png";
+import linux_img from "../images/linux.png";
+import mongo_img from "../images/mongo.png";
+import mysql_img from "../images/mysql.png";
+import postgresql_img from "../images/postgresql.png";
+import react_img from "../images/react.png";
+import redux_img from "../images/redux.png";
+import node_img from "../images/node.png";
+import material_img from "../images/material.png";
+
+const StyledButtonGroup = styled(ButtonGroup)`
+    width: 300px;
+    height: 70px;
+    position: fixed;
+    right: 15%;
+    margin-top: 15px;
+
+    .nav-button {
+        color: white;
+        background-color: #2d3436;
+        border-radius: 30px;
+        box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.4);
+        &:hover {
+            background-color: rgba(0, 0, 0, 0.3);
+        }
+    }
+
+    @media (max-width: 680px) {
+        opacity: 0;
+        position: absolute;
+        left: -300%;
+        .nav-button {
+            display: hidden;
+        }
+    }
 `;
 
-const StyledHome = styled(Grid)`
+const MenuBar = styled(FontAwesomeIcon)`
+    position: fixed;
+    right: 15%;
+    margin-top: 60px;
+
     color: white;
+
+    @media (max-width: 679) {
+        display: flex;
+        position: initial;
+        opacity: 1;
+    }
+`;
+
+const StyledHome = styled.div`
+    display: flex;
+    color: white;
+
     background-image: url(${bgimage});
     background-size: cover;
+
+    width: 100vw;
+    height: 100vh;
+
+    .home-item {
+        margin: auto;
+    }
+`;
+
+const StyledTypical = styled(Typical)`
+    font-size: calc(1em + 3vw);
+    text-align: center;
+    align-self: center;
+    justify-self: center;
+    text-shadow: 3px 3px 5px rgba(0, 0, 0, 0.4);
+    padding: 1rem;
+    background-color: black;
+
+    @media (max-width: 680px) {
+        font-size: calc(0.5em + 3vw);
+    }
+
+    @media (max-width: 320px) {
+        font-size: calc(0.3em + 3vw);
+    }
+`;
+
+const StyledAbout = styled.div`
+    display: grid;
+    grid-template-rows: repeat(5, 1fr);
+    grid-template-columns: repeat(5, 1fr);
+
+    background-color: lightgray;
     width: 100vw;
     height: 100vh;
 `;
 
-const StyledAbout = styled(Grid)`
-    background-color: grey;
+const AboutPage = styled.div`
+    grid-column: 1 / 6;
+    grid-row: 1 / 3;
+    display: flex;
+
+    background-color: white;
+`;
+
+const DetailAbout = styled.div`
+    /* border: 1px solid black; */
+    letter-spacing: 0.08rem;
+    line-height: 1.5;
+    text-align: ${(props) => (props.introduce ? "left" : "center")};
+    font-size: ${(props) => (props.introduce ? "1.2rem" : "3rem")};
+    flex-grow: ${(props) => (props.introduce ? "1" : "1")};
+    flex-basis: ${(props) => (props.introduce ? "60vw" : "30vw")};
+    margin: 10vw;
+    align-self: center;
+    justify-self: center;
+
+    @media (max-width: 770px) {
+        font-size: ${(props) =>
+            props.introduce ? `calc(0.5em + 1vw)` : `calc(1.5em + 2vw)`};
+    }
+
+    @media (max-width: 390px) {
+        font-size: ${(props) =>
+            props.introduce ? `calc(0.5em + 0.5vw)` : `calc(1.5em + 2vw)`};
+    }
+`;
+
+const SkillPage = styled.div`
+    display: flex;
+    grid-column: 1 / 6;
+    grid-row: 3 / 6;
+
+    background-color: #c7ecee;
+`;
+
+const SkillDetail = styled.div`
+    /* border: 1px solid black; */
+    font-size: 3rem;
+    flex-grow: ${(props) => (props.left ? "1" : "1")};
+    flex-shrink: ${(props) => (props.left ? 0 : 1)};
+    align-self: center;
+    justify-self: center;
+    text-align: center;
+
+    .skill-container {
+        display: flex;
+        justify-content: center;
+        margin-bottom: 1rem;
+    }
+
+    .skill-child {
+        border: 3px solid rgba(0, 0, 0, 0.5);
+        padding: 10px;
+        border-radius: 30px;
+        width: 70px;
+        height: 70px;
+        background-position: center;
+        background-size: cover;
+        margin-right: 10px;
+
+        @media (max-height: 950px) {
+            width: 50px;
+            height: 50px;
+        }
+
+        @media (max-width: 480px) {
+            width: 30px;
+            height: 30px;
+        }
+
+        @media (max-width: 390px) {
+            width: 20px;
+            height: 20px;
+            margin-right: 5px;
+        }
+    }
+`;
+
+const StyledProject = styled.div`
+    display: flex;
+    background-color: #ffbe76;
     width: 100vw;
     height: 100vh;
 `;
-
-const StyledSkill = styled(Grid)`
-    background-color: blue;
-    width: 100vw;
-    height: 100vh;
-`;
-
-const StyledProject = styled(Grid)`
-    background-color: green;
-    width: 100vw;
-    height: 100vh;
-`;
-
-function NavMenu() {
-    return (
-        <nav>
-            <a href="#page-1">1</a>
-            <a href="#page-2">2</a>
-            <a href="#page-3">3</a>
-        </nav>
-    );
-}
 
 export default function Page() {
+    const HomeRef = useRef();
+    const AboutRef = useRef();
+    const SkillsRef = useRef();
+    const ProjectRef = useRef();
+
+    function NavMenu() {
+        return (
+            <StyledButtonGroup>
+                <Button
+                    className="nav-button"
+                    variant="contained"
+                    color="primary"
+                    onClick={() => {
+                        window.scrollTo(0, HomeRef.current.offsetTop);
+                    }}
+                >
+                    Home
+                </Button>
+                <Button
+                    className="nav-button"
+                    variant="contained"
+                    color="primary"
+                    onClick={() => {
+                        window.scrollTo(0, AboutRef.current.offsetTop);
+                    }}
+                >
+                    About
+                </Button>
+                <Button
+                    className="nav-button"
+                    variant="contained"
+                    color="primary"
+                    onClick={() => {
+                        window.scrollTo(0, SkillsRef.current.offsetTop);
+                    }}
+                >
+                    Skills
+                </Button>
+                <Button
+                    className="nav-button"
+                    variant="contained"
+                    color="primary"
+                    onClick={() => {
+                        window.scrollTo(0, ProjectRef.current.offsetTop);
+                    }}
+                >
+                    Project
+                </Button>
+            </StyledButtonGroup>
+        );
+    }
+
+    function ShortMenu() {
+        return (
+            <React.Fragment>
+                <MenuBar icon={faBars} size="3x" />
+            </React.Fragment>
+        );
+    }
+
+    function MainContents() {
+        return (
+            <React.Fragment>
+                <StyledHome ref={HomeRef}>
+                    <div className="home-item">
+                        <StyledTypical
+                            steps={[
+                                "안녕하세요",
+                                1000,
+                                "안녕하세요 이민수의",
+                                500,
+                                "안녕하세요 이민수의 포트폴리오 입니다",
+                                500,
+                            ]}
+                            wrapper="p"
+                        />
+                    </div>
+                </StyledHome>
+
+                <StyledAbout>
+                    <AboutPage ref={AboutRef}>
+                        <DetailAbout>About Me</DetailAbout>
+                        <DetailAbout introduce>
+                            안녕하세요!! 풀스택 개발자를 꿈꾸는 경영학을 전공한
+                            이민수 입니다. 우연한 기회로 파이썬을 접한 후
+                            프로그래밍에 흥미를 느끼고 혼자서 이것 저것 공부하다
+                            보니 어느덧 개발자를 꿈꾸게 되었습니다. 비록
+                            전공자는 아니지만, 누구 보다 열심히 공부하고 있다고
+                            자부합니다. 혼자서 공부 해서 비록 부족한 점이 많다고
+                            생각하지만 부족한 부분을 매꾸는 방법을 체득했습니다.
+                            열심히 하고 잘하는 개발자가 되고 싶습니다!
+                        </DetailAbout>
+                    </AboutPage>
+                    <SkillPage ref={SkillsRef}>
+                        <SkillDetail left>Skills</SkillDetail>
+                        <SkillDetail right>
+                            <div className="skill-container">
+                                <div
+                                    className="skill-child"
+                                    style={{
+                                        backgroundImage: `url(${js_img})`,
+                                    }}
+                                />
+                                <div
+                                    className="skill-child"
+                                    style={{
+                                        backgroundImage: `url(${python_img})`,
+                                    }}
+                                ></div>
+                            </div>
+                            <div className="skill-container">
+                                <div
+                                    className="skill-child"
+                                    style={{
+                                        backgroundImage: `url(${html_img})`,
+                                    }}
+                                />
+                                <div
+                                    className="skill-child"
+                                    style={{
+                                        backgroundImage: `url(${css_img})`,
+                                    }}
+                                ></div>
+                                <div
+                                    className="skill-child"
+                                    style={{
+                                        backgroundImage: `url(${bootstrap_img})`,
+                                    }}
+                                ></div>
+                                <div
+                                    className="skill-child"
+                                    style={{
+                                        backgroundImage: `url(${material_img})`,
+                                    }}
+                                ></div>
+                            </div>
+                            <div className="skill-container">
+                                <div
+                                    className="skill-child"
+                                    style={{
+                                        backgroundImage: `url(${react_img})`,
+                                    }}
+                                ></div>
+                                <div
+                                    className="skill-child"
+                                    style={{
+                                        backgroundImage: `url(${node_img})`,
+                                    }}
+                                ></div>
+                                <div
+                                    className="skill-child"
+                                    style={{
+                                        backgroundImage: `url(${django_img})`,
+                                    }}
+                                ></div>
+                                <div
+                                    className="skill-child"
+                                    style={{
+                                        backgroundImage: `url(${react_native_img})`,
+                                    }}
+                                ></div>
+                            </div>
+                            <div className="skill-container">
+                                <div
+                                    className="skill-child"
+                                    style={{
+                                        backgroundImage: `url(${mongo_img})`,
+                                    }}
+                                ></div>
+                                <div
+                                    className="skill-child"
+                                    style={{
+                                        backgroundImage: `url(${mysql_img})`,
+                                    }}
+                                ></div>
+                                <div
+                                    className="skill-child"
+                                    style={{
+                                        backgroundImage: `url(${postgresql_img})`,
+                                    }}
+                                ></div>
+                            </div>
+                            <div className="skill-container">
+                                <div
+                                    className="skill-child"
+                                    style={{
+                                        backgroundImage: `url(${linux_img})`,
+                                    }}
+                                ></div>
+                                <div
+                                    className="skill-child"
+                                    style={{
+                                        backgroundImage: `url(${git_img})`,
+                                    }}
+                                ></div>
+                                <div
+                                    className="skill-child"
+                                    style={{
+                                        backgroundImage: `url(${redux_img})`,
+                                    }}
+                                ></div>
+                            </div>
+                        </SkillDetail>
+                    </SkillPage>
+                </StyledAbout>
+
+                <StyledProject ref={ProjectRef}>
+                    <div>Project</div>
+                </StyledProject>
+            </React.Fragment>
+        );
+    }
+
     return (
-        <StyledDiv>
+        <div>
             <NavMenu />
-            <StyledHome container id="page-1">
-                <Grid item>
-                    <Typical
-                        steps={[
-                            "안녕하세요",
-                            1000,
-                            "안녕하세요 이민수의",
-                            500,
-                            "안녕하세요 이민수의 포트폴리오 입니다",
-                            500,
-                        ]}
-                        wrapper="p"
-                    />
-                </Grid>
-            </StyledHome>
-
-            <StyledAbout container id="page-2">
-                <Grid item>About</Grid>
-            </StyledAbout>
-
-            <StyledSkill container id="page-3">
-                <Grid item>Skills</Grid>
-            </StyledSkill>
-
-            <StyledProject container>
-                <Grid item>Project</Grid>
-            </StyledProject>
-        </StyledDiv>
+            <ShortMenu />
+            <MainContents />
+        </div>
     );
 }
